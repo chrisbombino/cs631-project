ada344569e570d59d4ca9c9a4772a195b798e12e
Shahzeb Naveed <14beesnaveed@seecs.edu.pk>
2021-04-17 00:05:00 -0400

    Multiple changes for es and kafka

--- scripts/consumer_elasticsearch.py
+++ scripts/consumer_elasticsearch.py
@@ -4,12 +4,13 @@ from json import loads
 from uuid import uuid4
 
 # arguments
-processed_topic_name = 'processed_tweets'
-consumer_group_id = 'elasticsearch-consumers'
+source_topic_name = 'analyzed_tweets_106'
+consumer_group_id = 'elasticsearch_consumers'
+index_name = 'tweets_106'
 
 # init consumer
 consumer = KafkaConsumer(
-     processed_topic_name,
+     source_topic_name,
      bootstrap_servers=['localhost:9092'],
      auto_offset_reset='earliest',
      enable_auto_commit=True,
@@ -25,10 +26,17 @@ for message in consumer:
      # overwrite message with its value and preprocess text
      message = message.value.copy()
 
-    # TODO: Copy code from christopher to save onto elasticsearch
-    id = str(uuid4())
-    res = es.index(index='processed_tweets', id=id, body=message)
+     print("===============")
+     print(message)
 
-    print("===============")
-    print(message)
-    #print(res['result'])
+     # for elasticsearch to pick-up timestamps, have to specify a _timestamp field
+     # not sure if it looks for _ prefix for metadata of the document or
+     # the field name has to contain a "timestamp" substring.
+     #message["date"] = message["created_at"]
+     message["_timestamp"] = message["created_at"]
+
+     # write onto elasticsearch index
+     id = str(uuid4())
+     res = es.index(index=index_name, id=id, body=message)
+
+
